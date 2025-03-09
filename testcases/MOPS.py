c9940daf733ec66a98792da224d3fd073a11d59f
Sauerland <vsauerland@geomar.de>
2025-03-09 21:16:53 +0100

    na

--- testcases/MOPS.py
+++ testcases/MOPS.py
@@ -8,12 +8,12 @@ import fabmos.input.riverlist
 
 import pandas as pd
 
-tm_config_dir = "/Users/vsauerland/MITgcm_2.8deg"  # directory with a TM configuration from http://kelvin.earth.ox.ac.uk/spk/Research/TMM/TransportMatrixConfigs/
+tm_config_dir = "."  # directory with a TM configuration from http://kelvin.earth.ox.ac.uk/spk/Research/TMM/TransportMatrixConfigs/
 calendar = "360_day"  # any valid calendar recognized by cftime, see https://cfconventions.org/cf-conventions/cf-conventions.html#calendar
 
 script_dir = os.path.dirname(__file__)
 fabm_yaml = os.path.join(
-    script_dir, "fabm_with_runoff.yaml"
+    script_dir, "fabm.yaml"
 )
 
 domain = fabmos.transport.tmm.create_domain(tm_config_dir)
@@ -22,22 +22,7 @@ sim = fabmos.transport.tmm.Simulator(domain, calendar=calendar, fabm=fabm_yaml)
 
 sim.fabm.get_dependency("mole_fraction_of_carbon_dioxide_in_air").set(280.0)
 
-# VS: Jorns suggestion to have same annual air pressure forcing as with atmosp_ files
-#     in the PETSc-TMM-MOPS version (see Jorns mail at August 29, 2024)
-airp = fabmos.input.from_nc("/Users/vsauerland/tmm_matlab_code/OCMIP_Utils/Data/gasx_ocmip2.nc", "P", decode_times=False)
-airp.coords["MONTH_REG"] = fabmos.transport.tmm.climatology_times(calendar)
-sim.fabm.get_dependency("surface_air_pressure").set(airp * 101325.0,climatology=True)
-
-# VS: deactivating dilution/concentration of tracers by precipitation/evaporation
-#     this keeps uniformly distributet BGC tracers uniformly distributed
-#     if the corresponding BGC processes are deactivated
-#     (see Jorns mail at August 24, 2024)
-# VS: but we have to switch it off for carbon
-#     to yield the effect of the term co2emp in BGC_MODEL.F
-#     Jorn mailed me how to do it (November 7, 2024, second approach)
-for tracer in sim.tracers:
-   if tracer.name != 'carbon_c':
-      tracer.precipitation_follows_target_cell = True
+sim.fabm.get_dependency("surface_air_pressure").set(101325.0)
 
 # Load rivers from https://doi.org/10.1029/96JD00932
 rivers = pd.read_fwf(
